baf91973a71e37672e997550c72e0c254d36d4ec
Harry Daniel Gurth Angeles <harry@tolly.io>
2025-03-19 11:16:09 +0100

    Added a business component along with a CSS file for styling purposes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Business from './components/Business';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Harry
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Business />
     </div>
   );
 }
